2fb495aecf2cc9aa34963363892324ddbad75c8e
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2024-12-25 19:34:55 -0800

    Add objects to store data. Refactor for exports
    1. Create userSearch and weatherReport object to store and transfer data
    2. Refactor functions to validate search parameters, and to allow for
    exports.

--- src/modules/geolocation.js
+++ src/modules/geolocation.js
@@ -1,28 +1,84 @@
-async function getUserCity(lat, long) {
-  console.log(lat + "," + long);
-  const response = await fetch(
-    `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&apiKey=ffb7959793ea43d885855cda974d3e3a`,
-    { method: "GET" }
-  );
+import { initializeWeatherDisplayPage } from "./weather-display";
 
-  const json = await response.json();
-  const city = await json.features[0].properties.city;
-  console.log(json);
-  console.log(city);
-}
+let userSearch = {
+  city: "none",
+};
+let weatherReport = {};
 
-function getUserCoordinates() {
+async function getUserCoordinates() {
+  // check if user allows location service
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
-      const lat = position.coords.latitude;
-      const long = position.coords.longitude;
-      console.log(lat + "," + long);
-      getUserCity(lat, long);
+    const permission = await navigator.permissions.query({
+      name: "geolocation",
     });
+
+    if (permission.state === "granted") {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        weatherReport.latitude = lat;
+        weatherReport.longitude = long;
+        getUserCity(lat, long);
+      });
+    } else if (permission.state === "denied") {
+      console.error(
+        "Access to location services has been denied. Please enable location permissions in your browser or device settings to use this feature."
+      );
+    }
   } else {
-    console.error("Geolocation is not supported by this browser.");
+    console.error("Geolocation service not available for browser.");
   }
 }
 
-getUserCoordinates();
+// get user city from lat and long coordinates using Geoapify API
+
+async function getUserCity(lat, long) {
+  let response;
+  try {
+    response = await fetch(
+      `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&apiKey=ffb7959793ea43d885855cda974d3e3a`,
+      { method: "GET" }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+
+  const json = await response.json();
+  const city = await json.features[0].properties.city;
+  weatherReport.city = city;
+  getUserWeather(city, lat, long);
+}
+
+// Get Current Weather from Visual Crossing API
+async function getUserWeather(city, lat, long) {
+  let response;
+  // If lat and long are available fetch weather using coords for accuracy
+  if (lat && long) {
+    try {
+      console.log("Using coords");
+      response = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${long}?unitGroup=us&key=KN8KKWLCB443EKTSE8MTPE76Z`
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    // else fetch using city name
+    try {
+      console.log(city);
+      const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=KN8KKWLCB443EKTSE8MTPE76Z&contentType=json`;
+      console.log("Using city" + city + "at" + url);
+      response = await fetch(url);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  if (response) {
+    const weatherJSON = await response.json();
+    weatherReport.report = weatherJSON;
+    initializeWeatherDisplayPage();
+  }
+}
+
+export { userSearch, weatherReport, getUserCoordinates, getUserWeather };
