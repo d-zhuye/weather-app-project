9b4e44fca53da0fd6123f7683aec79853a75c09c
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2025-01-02 14:43:40 -0800

    Reconfigure modules

--- webpack.common.js
+++ webpack.common.js
@@ -2,35 +2,33 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
   entry: {
     app: "./src/index.js",
   },
   output: {
-    filename: "weather.bundle.js",
+    filename: "main.bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      title: "Production",
     }),
   ],
   module: {
     rules: [
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        },
-        ,
-        {
-          test: /\.html$/i,
-          loader: "html-loader",
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset/resource",
-        } 
-    ]
-  }
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
